ce5b46087c581ea366437e229d9a41b5bf352767
Anton <abantonoff@gmail.com>
2021-08-02 05:16:11 +0300

    Add database initializer

---
+++ Hotel_DAL/EF/DbUserInitializer.cs
@@ -0,0 +1,61 @@
+﻿using Hotel_DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_DAL.EF
+{
+    public class DbUserInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
+        {
+            string AdminNumber = "0684387784";
+            string AdminPassword = "111111";
+
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("Admin"));
+            }
+            if (await roleManager.FindByNameAsync("Guest") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole<int>("Guest"));
+            }
+            if (await userManager.FindByNameAsync(AdminNumber) == null)
+            {
+                User admin = new User()
+                {
+                    Name = "Anton",
+                    Surname = "Antonov",
+                    Address = "MySrteet",
+                    PhoneNumber = "0684387784",
+                    UserName = "0684387784",
+                };
+                IdentityResult result = await userManager.CreateAsync(admin, AdminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                    await userManager.AddToRoleAsync(admin, "Guest");
+                }
+
+                User user = new User()
+                {
+                    Name = "Misha",
+                    Surname = "Antonov",
+                    Address = "Other Street",
+                    PhoneNumber = "1111",
+                    UserName = "1111",
+                };
+                IdentityResult result2 = await userManager.CreateAsync(user, "222222");
+                if (result2.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Guest");
+                }
+
+
+            }
+        }
+    }
+}
--- Hotel_DAL/EF/HotelContext.cs
+++ Hotel_DAL/EF/HotelContext.cs
@@ -14,7 +14,7 @@ namespace Hotel_DAL.EF
     {
         public HotelContext(DbContextOptions<HotelContext> options) : base(options)
         {
-
+            Database.EnsureCreated();
         }
 
         internal DbSet<Category> Categories { get; set; }
@@ -25,12 +25,178 @@ namespace Hotel_DAL.EF
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasData(
-                new User[]
-                {
-                //new User { Id=1, Name="Tom", Age=23},
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Category>().HasData(
+                new Category[]
+                {
+                    new Category()
+                    {
+                        Id=1,
+                        Name = "Стандарт",
+                        Bed = 1,
+                        Description = "Описание категории стандарт с одной кроватью"
+
+                    },
+                    new Category()
+                    {
+                        Id=2,
+                        Name = "Стандарт",
+                        Bed = 2,
+                        Description = "Описание категории стандарт с двумя кроватями"
+
+                    },
+                    new Category()
+                    {
+                        Id=3,
+                        Name = "Люкс",
+                        Bed = 1,
+                        Description = "Описание категории люкс с одной кроватью"
+
+                    },
+                    new Category()
+                    {
+                        Id=4,
+                        Name = "Люкс",
+                        Bed = 2,
+                        Description = "Описание категории люкс с двумя кроватями"
+                    },
                 });
+
+                modelBuilder.Entity<Room>().HasData(
+                new Room[]
+                {
+                    new Room()
+                    {
+                        Id=1,
+                        Name = "101",
+                        CategoryId = 1,
+                        Active = true
+                    },
+                 
+                    new Room()
+                    {
+                        Id=2,
+                        Name = "102",
+                        CategoryId = 2,
+                        Active = true
+                    },
+                 
+                    new Room()
+                    {
+                        Id=3,
+                        Name = "201",
+                        CategoryId = 3,
+                        Active = true                  
+                    },
+                 
+                    new Room()
+                    {
+                        Id=4,
+                        Name = "202",
+                        CategoryId = 4,
+                        Active = true
+                    }
+                });
+
+                
+
+                //modelBuilder.Entity<Booking>().HasData(
+                //new Booking[]
+                //{
+                //    new Booking()
+                //    {
+                //        Id = 1,
+                //        UserId = 2,
+                //        RoomId = 1,
+                //        Set = false,
+                //        BookingDate = new DateTime(2021, 3, 5),
+                //        EnterDate = new DateTime(2021, 3, 6),
+                //        LeaveDate = new DateTime(2021, 3, 10),
+                //    },
+     
+                //    new Booking()
+                //    {
+                //        Id = 1,
+                //        UserId = 2,
+                //        RoomId = 1,
+                //        Set = true,
+                //        BookingDate = new DateTime(2021, 3, 15),
+                //        EnterDate = new DateTime(2021, 3, 16),
+                //        LeaveDate = new DateTime(2021, 3, 20),
+                //    },
+     
+                //    new Booking()
+                //    {
+                //        Id = 1,
+                //        UserId = 2,
+                //        RoomId = 2,
+                //        Set = true,
+                //        BookingDate = new DateTime(2021, 4, 2),
+                //        EnterDate = new DateTime(2021, 4, 10),
+                //        LeaveDate = new DateTime(2021, 4, 20),
+                //    },
+     
+                //    new Booking()
+                //    {
+                //        Id = 1,
+                //        UserId = 2,
+                //        RoomId = 3,
+                //        Set = true,
+                //        BookingDate = new DateTime(2021, 4, 15),
+                //        EnterDate = new DateTime(2021, 4, 16),
+                //        LeaveDate = new DateTime(2021, 5, 20),
+                //    },
+                //});
+
+                modelBuilder.Entity<PriceCategory>().HasData(
+                new PriceCategory[]
+                {
+                    new PriceCategory()
+                    {
+                        Id = 1,
+                        CategoryId = 1,
+                        Price = 100,
+                        StartDate = new DateTime(2017, 3, 15),
+                        EndDate = new DateTime(2022, 3, 15),
+                    },
+         
+                    new PriceCategory()
+                    {
+                        Id = 2,
+                        CategoryId = 2,
+                        Price = 200,
+                        StartDate = new DateTime(2017, 3, 15),
+                        EndDate = new DateTime(2022, 3, 15),
+                    },
+         
+                    new PriceCategory()
+                    {
+                        Id = 3,
+                        CategoryId = 3,
+                        Price = 300,
+                        StartDate = new DateTime(2017, 3, 15),
+                        EndDate = new DateTime(2021, 4, 20),
+                    },
+         
+                    new PriceCategory()
+                    {
+                        Id = 4,
+                        CategoryId = 3,
+                        Price = 1000,
+                        StartDate = new DateTime(2021, 4, 20),
+                        EndDate = new DateTime(2022, 3, 15),
+                    },
+         
+                    new PriceCategory()
+                    {
+                        Id = 5,
+                        CategoryId = 4,
+                        Price = 1400,
+                        StartDate = new DateTime(2017, 3, 15),
+                        EndDate = new DateTime(2022, 3, 15),
+                    },
+                });            
         }
 
     }
